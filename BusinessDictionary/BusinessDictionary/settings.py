eb8b04995ceeb8e1165b84ad1930eaa7d48ef669
Cristhian <31833615+cristhian2121@users.noreply.github.com>
2018-07-12 22:30:19 -0500

    ajustes

--- BusinessDictionary/BusinessDictionary/settings.py
+++ BusinessDictionary/BusinessDictionary/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'BusinessDictionary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_Ruteando',
+        'USER':'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1', # Or an IP Address that your DB is hosted on
+        'PORT': '',
     }
 }
 
